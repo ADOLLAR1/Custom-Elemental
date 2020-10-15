5298c1cc762d8265844f652b36fcf6acee0d0411
ADOLLAR1 <adollar@stcharlessd.org>
2020-10-15 08:15:01 -0500

    Fixed Bug

--- sketch.js
+++ sketch.js
@@ -85,10 +85,10 @@ function draw() {
     background(191);
     let j = 0
     for(i=0;i<elements.length; i++) { //Draw the elements to the canvas in a grid pattern
-        elements[i].setPosition(createVector(Math.floor((j)/950),((j)*50)%950));
+        elements[i].setPosition(createVector(Math.floor((j*50)/800)*50,((j)*50)%800));
         elements[i].draw();
         elements[i].offset = elements[i].offset + 0.0025;
-        if (elements[i].votes>=10&&elements[i].unlocked==1) {j+=1;}
+        if (elements[i].votes>=10&&elements[i].unlocked!=0) {j+=1;}
     }
     if (createGui.visible) { //Draw the Create GUI only if visible
         createGui.draw();
